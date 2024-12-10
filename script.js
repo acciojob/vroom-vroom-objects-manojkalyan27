a628409a7de3abd241e392f64596dc9b63393ea9
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-12-11 00:00:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
-// Car Constructor Function
+
 function Car(make, model) {
   this.make = make;
   this.model = model;
 }
 
-// Add getMakeModel method to Car prototype
 Car.prototype.getMakeModel = function () {
   return `${this.make} ${this.model}`;
 };
 
-// SportsCar Constructor Function
 function SportsCar(make, model, topSpeed) {
-  // Call the Car constructor to initialize make and model
   Car.call(this, make, model);
   this.topSpeed = topSpeed;
 }
 
-// Inherit the Car prototype in SportsCar
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
 
-// Add getTopSpeed method to SportsCar prototype
 SportsCar.prototype.getTopSpeed = function () {
   return this.topSpeed;
 };
 
-// Example usage
 const car = new Car("Toyota", "Corolla");
-console.log(car.getMakeModel()); // Output: "Toyota Corolla"
+console.log(car.getMakeModel()); 
 
 const sportsCar = new SportsCar("Ferrari", "488 Spider", 211);
-console.log(sportsCar.getMakeModel()); // Output: "Ferrari 488 Spider"
-console.log(sportsCar.getTopSpeed()); // Output: 211
+console.log(sportsCar.getMakeModel());
+console.log(sportsCar.getTopSpeed()); 
